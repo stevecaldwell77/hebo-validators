07412b9ae95eb6d624cf2ead53033ef462d83cbf
Stephen Caldwell <steve@campusexplorer.com>
2018-10-11 14:44:04 -0700

    chore: customize lass skeleton

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 class Script {
-  constructor(config) {
-    config = { ...config };
-    this._name = config.name || 'script';
+    constructor(config) {
+        config = { ...config };
+        this._name = config.name || 'script';
 
-    this.renderName = this.renderName.bind(this);
-  }
+        this.renderName = this.renderName.bind(this);
+    }
 
-  renderName() {
-    return this._name;
-  }
+    renderName() {
+        return this._name;
+    }
 }
 
 module.exports = Script;
